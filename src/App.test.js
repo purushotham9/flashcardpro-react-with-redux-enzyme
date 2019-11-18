4883df491fb9433e2b25a6c850e3c4fbef7e7b60
purushotham9 <purushothamgire@gmail.com>
2019-11-18 19:34:28 +0530

    create a react project with redux and enzyme test cases

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {shallow, configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
+configure({adapter: new Adapter()});
+
+describe('App', ()=>{
+  const app = shallow(<App />);
+
+  it('renders the `FlashcardPro` title', ()=>{
+    expect(app.find('h1').text()).toEqual('Flashcardpro')
+  })
+  it('renders the StackList', ()=>{
+    expect(app.find('Connect(StackList)').exists()).toBe(true);
+  })
+  it('renders the a link to create to create new stacks', ()=>{
+    expect(app.find('Link h2').text()).toEqual('Create a New StackList')
+  })
 });
+
--- src/index.js
+++ src/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Stack from './components/Stack'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux';
+import rootReducer from './reducres';
+import { setStack } from './actions';
+import { Provider } from 'react-redux';
+import StackForm from './components/StackForm'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(setStack({ id: 0, title: 'example', cards: [] }))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/stack" component={Stack} />
+                <Route exact path="/stack-form" component={StackForm} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
